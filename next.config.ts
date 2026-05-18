9fc20109a54f1a74f3bf513374bce014767e6f21
Alexbang <alex184893723@163.com>
2026-05-18 11:23:51 +0800

    fix:nextCongif

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   output: 'export', // 开启静态导出，构建后会生成 dist 目录
+  trailingSlash: true, // 可选，让路由更适配静态托管
 };
 
 export default nextConfig;
