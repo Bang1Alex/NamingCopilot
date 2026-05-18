d818a58ac8608e0056ba337069017ea508fdab21
Alexbang <alex184893723@163.com>
2026-05-18 11:30:13 +0800

    feat+perf:add gitignore file and  perf nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-   output: 'export', // 开启静态导出，构建后会生成 dist 目录
-  trailingSlash: true, // 可选，让路由更适配静态托管
+  output: 'export',        // 生成 dist 目录（解决部署报错）
+  distDir: 'dist',        // 明确输出 dist
+  trailingSlash: true,     // 静态托管路由稳定
+  images: { unoptimized: true }, // 静态导出必须关
+  env: {
+    ARK_API_KEY: process.env.ARK_API_KEY,
+    ARK_BASE_URL: process.env.ARK_BASE_URL,
+    ARK_MODEL: process.env.ARK_MODEL,
+  },
 };
 
 export default nextConfig;
